5afd5705b5b81a87333614ebcf99eb9af9333cba
Soulza <soulzaltd@gmail.com>
2025-02-13 19:56:33 +0000

    Salvando Arquivo.

---
+++ 1. Elabora um algoritimo.py
@@ -0,0 +1,18 @@
+import os 
+
+os.system("clear") 
+
+numero = int(input("Digite um numero: "))
+
+if numero > 10:
+    print("É MAIOR QUE 10!")
+else:    
+    print("NÃO É MAIOR QUE 10!")
+
+
+
+
+
+
+
+
