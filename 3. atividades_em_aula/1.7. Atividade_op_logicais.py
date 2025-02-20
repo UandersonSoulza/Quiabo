9cd0e580dcdf2869b9ac45aac7a0ffd3e1559855
Soulza <soulzaltd@gmail.com>
2025-02-20 19:17:09 +0000

    Salvando arquivos

---
+++ 3. atividades_em_aula/1.7. Atividade_op_logicais.py
@@ -0,0 +1,31 @@
+import os
+os.system ("clear")
+
+
+# Solicitando dados para o usuario
+ob = int(input("Digite sua idade: "))
+
+# Opção 1
+ 
+# Verificando os dados cadastrados com os dados informados pelo usuario
+if ob < 18 or ob > 65:
+    print ("Você não irá votar!")
+else:
+    print ("Você irá votar!")
+
+# Opção 2    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
