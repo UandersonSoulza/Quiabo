f0d127317eb22e262fa85e96f6bc91cffc40822f
Soulza <soulzaltd@gmail.com>
2025-04-03 20:02:14 +0000

    SALVO

---
+++ 7. Funções/3.3. def_media_aluno.py
@@ -0,0 +1,22 @@
+import os
+os.system ("cls ||clear ")
+
+def calc(media):
+    media == (nu1 + nu2) / 2
+
+def medidi(media):
+    if media >= 7:
+        print ("Aprovado!")
+    else:
+        print ("Reprovado!")
+
+
+
+nu1 = int(input("Digite um número: "))
+nu2 = int(input("Digite um número: "))
+calc(nu1, nu2) 
+medidi (nu1, nu2)
+
+print(f"{medidi} {calc}")
+
+
