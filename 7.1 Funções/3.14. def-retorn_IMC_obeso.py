e516a601b0268a33795a30e273360afdb5038d0b
Soulza <soulzaltd@gmail.com>
2025-04-10 18:58:44 +0000

    Salvo

--- 7.1 Funções/3.14. def-retorn_IMC_obeso.py
+++ 7.1 Funções/3.14. def-retorn_IMC_obeso.py
@@ -10,8 +10,10 @@ alt = float(input("Escreva sua altura: "))
 
 resul = imc (peso, alt)
 
+print (f"Valor do IMC: {resul:.2F}") 
+    
 if resul < 18.5:
-    print ("Abaixo do peso! Consulte um nutricionista para orientação.")
+    print ("\nAbaixo do peso! Consulte um nutricionista para orientação.")
 elif resul < 25:
     print ("\nPeso normal. Manter hábitos saudáveis!")
 elif resul < 30:
@@ -22,4 +24,3 @@ elif resul < 40:
     print ("\nObesidade de grau 2! Consulte um médico para avaliação e orientação.")
 else:
     print ("\nObesidade de grau 3! Busque assistência médica imediatamente.")
-    
