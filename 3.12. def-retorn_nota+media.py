fdce50f55da19f309d529d844a721e545dfc79a0
Soulza <soulzaltd@gmail.com>
2025-04-10 18:37:02 +0000

    Salvo

---
+++ 3.12. def-retorn_nota+media.py
@@ -0,0 +1,15 @@
+import os
+os.system ("cls||clear")
+
+n1 = 0
+
+def medidi (n1):
+    calc = n1 / 3
+    return calc
+
+for i in range(3):
+    n1 += int(input(f"Digite sua {1+i}º nota: "))
+
+media = medidi (n1)
+
+print (f"\nResultado da média: {media}")
